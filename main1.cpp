3cb9ab0576f5640b19748147b2e3468a01cff274
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2022-05-13 10:27:18 -0800

    volume (cylinder)

---
+++ main1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//volume of a cylinder pie*r*r*h;
+int main() {
+	float pie = 3.142;
+	float radius, height, vol;
+	
+	cout<<"\t\tWelcome to frads maths solutions!!: ";
+	cout<<"\nEnter the radius of the cylinder: ";
+	cin>>radius;
+	
+	cout<<"\nEnter the radius of the cylinder: ";
+	cin>>radius;
+	
+	cout<<"\nEnter the height of the cylinder: ";
+	cin>>height;
+	
+	vol = pie * radius * radius * height;
+	
+	cout<<"\nThe volume of the cylinder is =  "<<vol;
+	cout<<"\nThank you!!!Your problem has been solved successfully........";
+}
+
